7f8c4087dc54f8b5e20d07ed623eb68ec8f3e24b
gleb <gkebkasyak@gmail.com>
2019-11-29 23:53:34 +0300

    Updates

---
+++ precache-manifest.0d401ed27939318d5f7977f7c0d1df0b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16d4d64878b699340401c404e931087a",
+    "url": "/LyricFinderApp/index.html"
+  },
+  {
+    "revision": "e8bd7981c721474e1338",
+    "url": "/LyricFinderApp/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "054d24f86ebda5d09669",
+    "url": "/LyricFinderApp/static/css/main.8f5811d7.chunk.css"
+  },
+  {
+    "revision": "e8bd7981c721474e1338",
+    "url": "/LyricFinderApp/static/js/2.746bffdd.chunk.js"
+  },
+  {
+    "revision": "054d24f86ebda5d09669",
+    "url": "/LyricFinderApp/static/js/main.c345b340.chunk.js"
+  },
+  {
+    "revision": "9a12b78d15f5832b622d",
+    "url": "/LyricFinderApp/static/js/runtime-main.18442056.js"
+  }
+]);
